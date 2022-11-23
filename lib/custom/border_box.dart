6c2fe8ad061dbfac9f688f7c0a0a2e78e6b48206
Bamidele Ajewole <bamideledavid.femi@gmail.com>
2022-11-23 13:07:24 +0100

    Working on the landing page

---
+++ lib/custom/border_box.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:ui_design_1/utils/constants.dart';
+
+class BorderBox extends StatelessWidget {
+  const BorderBox({
+    Key? key,
+    required this.child,
+    this.padding = const EdgeInsets.all(0),
+    required this.width,
+    required this.height,
+  }) : super(key: key);
+
+  final Widget child;
+  final EdgeInsets? padding;
+  final double width, height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        color: colorWhite,
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(
+          color: colorGrey.withAlpha(40),
+          width: 2,
+        ),
+      ),
+      padding: padding,
+      child: Center(
+        child: child,
+      ),
+    );
+  }
+}
--- lib/screens/LandingPage.dart
+++ lib/screens/LandingPage.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:ui_design_1/custom/border_box.dart';
+import 'package:ui_design_1/utils/constants.dart';
 
 class LandingPage extends StatelessWidget {
   const LandingPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final Size size = MediaQuery.of(context).size;
+    final ThemeData themeData = Theme.of(context);
+    const double spacing = 15;
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: spacing),
+          child: SizedBox(
+            width: size.width,
+            height: size.height,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: spacing),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      BorderBox(
+                        width: 40,
+                        height: 40,
+                        child: Icon(
+                          Icons.menu_rounded,
+                          color: colorBlack,
+                        ),
+                      ),
+                      BorderBox(
+                        width: 40,
+                        height: 40,
+                        child: Icon(
+                          Icons.settings_outlined,
+                          color: colorBlack,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Text(
+                  'City',
+                  style: themeData.textTheme.bodyText2,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
